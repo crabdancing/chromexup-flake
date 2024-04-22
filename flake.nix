4f3d83c2bc2dbb7ed037c64c94f32b07674898a0
Alexandria P. <alxpettit@gmail.com>
2024-04-22 14:09:02 -0600

    ok module time soon

--- flake.nix
+++ flake.nix
@@ -11,28 +11,32 @@
   outputs = { self, nixpkgs, flake-utils, chromexup }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        
+        # Prepare the Python package using the setup.py in the chromexup repository
         pkgs = import nixpkgs {
           inherit system;
-        };
-        # Prepare the Python package using the setup.py in the chromexup repository
-        chromexup = pkgs.python3Packages.buildPythonApplication {
+          overlays = [
+            (self: super: {
+              chromexup = super.python3Packages.buildPythonApplication {
                 pname = "chromexup";
                 version = "unstable"; # Use 'unstable' as there is no version in the repo
                 src = chromexup;
-                propagatedBuildInputs = with pkgs.python3Packages; [ requests ];
+                propagatedBuildInputs = with super.python3Packages; [ requests ];
                 # You may need to add more Python dependencies depending on the actual needs
                 # You could also override 'doCheck' and other attributes as required
+              };
+            })
+          ];
         };
       in
       {
         # Provide Nix packages and apps using the package built above
-        # packages.chromexup = chromexup;
-        # apps.chromexup = pkgs.lib.mkApp {
-        #   inherit (chromexup) drv;
-        #   program = "${chromexup}/bin/chromexup";
-        # };
-        defaultPackage = chromexup;
+        packages.chromexup = pkgs.chromexup;
+        apps.chromexup = pkgs.lib.mkApp {
+          inherit (pkgs.chromexup) drv;
+          program = "${pkgs.chromexup}/bin/chromexup";
+        };
+        defaultPackage = pkgs.chromexup;
       });
 }
+
 
