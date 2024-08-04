63ecc768fce29dd4bf0ca797915fadf5c9eb2cee
Alexandria P. <alxpettit@gmail.com>
2024-08-03 18:45:28 -0600

    mkApp missing

--- flake.nix
+++ flake.nix
@@ -8,32 +8,34 @@
     chromexup-src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, chromexup-src }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let
-        # Prepare the Python package using the setup.py in the chromexup repository
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        chromexup = pkgs.callPackage ./pkg.nix { inherit  chromexup-src; };
-      in
-      {
-        # Provide Nix packages and apps using the package built above
-        packages.chromexup = chromexup;
-        apps.chromexup = pkgs.lib.mkApp {
-          inherit (chromexup) drv;
-          program = "${chromexup}/bin/chromexup";
-        };
-        packages.default = chromexup;
-
-      })) // (let
-        module = import ./module.nix { inherit chromexup-src; };
-      in {      
-        homeManagerModules = {
-          default = module;
-          chromexup = module;
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    chromexup-src,
+  }:
+    (flake-utils.lib.eachDefaultSystem (system: let
+      # Prepare the Python package using the setup.py in the chromexup repository
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      chromexup = pkgs.callPackage ./pkg.nix {inherit chromexup-src;};
+    in {
+      # Provide Nix packages and apps using the package built above
+      packages.chromexup = chromexup;
+      # broken?
+      # apps.chromexup = pkgs.lib.mkApp {
+      #   inherit (chromexup) drv;
+      #   program = "${chromexup}/bin/chromexup";
+      # };
+      packages.default = chromexup;
+    }))
+    // (let
+      module = import ./module.nix {inherit chromexup-src;};
+    in {
+      homeManagerModules = {
+        default = module;
+        chromexup = module;
+      };
+    });
 }
-
-
