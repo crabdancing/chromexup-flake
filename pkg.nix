fbd01b3e746043a35709567a7fa99625e02b18dd
Alexandria P. <alxpettit@gmail.com>
2025-12-05 12:20:34 -0700

    ok

--- pkg.nix
+++ pkg.nix
@@ -1,14 +1,14 @@
 {
   chromexup-src,
   pkgs,
-  setuptools,
+  python3Packages,
 }:
 pkgs.python3Packages.buildPythonApplication {
   pname = "chromexup";
   version = "unstable"; # Use 'unstable' as there is no version in the repo
   src = chromexup-src;
   propagatedBuildInputs = with pkgs.python3Packages; [requests];
-  build-system = [setuptools];
+  build-system = with python3Packages; [setuptools];
   # You may need to add more Python dependencies depending on the actual needs
   # You could also override 'doCheck' and other attributes as required
 }
